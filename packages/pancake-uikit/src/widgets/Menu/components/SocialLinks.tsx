c233e8d5663724c8ebe072a2f27de44cce9ec80e
Joan Domingo <joands@gmail.com>
2021-08-11 22:40:13 +0200

    Fix build issue

--- packages/pancake-uikit/src/widgets/Menu/components/SocialLinks.tsx
+++ packages/pancake-uikit/src/widgets/Menu/components/SocialLinks.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { SvgProps } from "../../../components/Svg";
 import Flex from "../../../components/Box/Flex";
-import Dropdown from "../../../components/Dropdown/Dropdown";
 import Link from "../../../components/Link/Link";
 import * as IconModule from "../icons";
 import { socials } from "../config";
@@ -18,27 +17,6 @@ const SocialLinks: React.FC = () => (
         style: { cursor: "pointer" },
       };
       const mr = index < socials.length - 1 ? "24px" : 0;
-      if (social.items) {
-        return (
-          <Dropdown
-            key={social.label}
-            position="top"
-            target={<Icon {...iconProps} mr={mr} />}
-          >
-            {social.items.map((item) => (
-              <Link
-                external
-                key={item.label}
-                href={item.href}
-                aria-label={item.label}
-                color="textSubtle"
-              >
-                {item.label}
-              </Link>
-            ))}
-          </Dropdown>
-        );
-      }
       return (
         <Link
           external
